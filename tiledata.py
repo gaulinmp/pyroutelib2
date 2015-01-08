d48421739e01ab2b9a2b643d8fc934d9160ba272
Mac Gaulin <gaulinmp+git@gmail.com>
2015-01-08 00:39:48 -0600

    Updated tiledata to pull from OSM API v0.6

--- tiledata.py
+++ tiledata.py
@@ -21,6 +21,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #---------------------------------------------------------------------------
 import os
+import pickle
 try:  # Python 3.x
   from urllib.request import urlretrieve
 except ImportError:  # Python 2.x
@@ -35,23 +36,34 @@ def DownloadLevel():
 def GetOsmTileData(z,x,y):
   """Download OSM data for the region covering a slippy-map tile"""
   if(x < 0 or y < 0 or z < 0 or z > 25):
-    print("Disallowed %d,%d at %d" % (x, y, z))
+    print("Disallowed (%d,%d) at zoom level %d" % (x, y, z))
     return
-  s,w,n,e = tileEdges(x,y,z)
   
   directory = 'cache/%d/%d/%d' % (z,x,y)
-  filename = '%s/data.osm' % (directory)
+  filename = '%s/data.osm.pkl' % (directory)
   if(not os.path.exists(directory)):
     os.makedirs(directory)
 
-  # /api/0.6/map?bbox=left,bottom,right,top
-  URL = 'http://api.openstreetmap.org/api/0.6/map?bbox={:0.4f},{:0.4f},{:0.4f},{:0.4f}'.format(w,s,e,n)
-  print(URL)
-   
-  if(not os.path.exists(filename)): # TODO: allow expiry of old data
-    urlretrieve(URL, filename)
-  return(filename)
+  if(z == DownloadLevel()):
+    # Download the data
+    s,w,n,e = tileEdges(x,y,z)
+    # /api/0.6/map?bbox=left,bottom,right,top
+    URL = 'http://api.openstreetmap.org/api/0.6/map?bbox={},{},{},{}'.format(w,s,e,n)
+
+     
+    if(not os.path.exists(filename)): # TODO: allow expiry of old data
+      urlretrieve(URL, filename)
+    return(filename)
+    
+  elif(z > DownloadLevel()):
+    # use larger tile
+    while(z > DownloadLevel()):
+      z = z - 1
+      x = int(x / 2)
+      y = int(y / 2)
+    return(GetOsmTileData(z,x,y))
+  return(None)
 
 if(__name__ == "__main__"):
   """test mode"""
-  print(GetOsmTileData(15, 16218, 10741))
+  print(GetOsmTileData(15, 7700, 13546))
