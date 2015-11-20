85ec1e99f242c9dc2ff7baa4a6fb476c2ff695c7
Pedro Palacios Avila <pedro_gpa@hotmail.com>
2015-11-19 23:34:32 -0500

    Fixed routeAsCSV

--- routeAsCSV.py
+++ routeAsCSV.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python
 #----------------------------------------------------------------
-# routeAsOsm - routes with OSM data, and generates a
-# GPX file containing the result
+# routeAsCSV - routes with OSM data, and generates a
+# CSV file containing the result
 #
 #------------------------------------------------------
-# Usage: 
-#  routeAsGpx.py [lat1] [lon1] [lat2] [lon2] [transport]
 #------------------------------------------------------
 # Copyright 2007-2008, Oliver White
 #
@@ -24,9 +22,9 @@
 #------------------------------------------------------
 from route import Router
 from loadOsm import LoadOsm
-  
-def routeToGpx(lat1,lon1,lat2,lon2, transport, description="", style="track"):
-  """Format a route (as list of nodes) into a GPX file"""
+
+def routeToCSV(lat1,lon1,lat2,lon2, transport):
+  """Format a route (as list of nodes)"""
   data = LoadOsm(transport)
 
   node1 = data.findNode(lat1,lon1)
@@ -35,53 +33,26 @@ def routeToGpx(lat1,lon1,lat2,lon2, transport, description="", style="track"):
   router = Router(data)
   result, route = router.doRoute(node1, node2)
   if result != 'success':
-    return
+    return("Fail")
 
   output = ''
-  output = output + "<?xml version='1.0'?>\n";
-  
-  output = output + "<gpx version='1.1' creator='pyroute' xmlns='http://www.topografix.com/GPX/1/1' xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xsi:schemaLocation='http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd'>\n"
-    
-  if(style == 'track'):
-    output = output + " <trk>\n"
-    output = output + "  <name>%s</name>\n" % description
-    output = output + "  <trkseg>\n"
-    count = 0;
-    for i in route:
-      node = data.rnodes[i]
-      output = output + "   <trkpt lat='%f' lon='%f'>\n" % ( \
-        node[0],
-        node[1])
-      output = output + "   </trkpt>\n"
-      count = count + 1
-    output = output + "  </trkseg>\n  </trk>\n</gpx>\n"
-
-  elif(style == 'route'):
-    output = output + " <rte>\n"
-    output = output + "  <name>%s</name>\n" % description
-    
-    count = 0;
-    for i in route:
-      node = data.rnodes[i]
-      output = output + "   <rtept lat='%f' lon='%f'>\n" % ( \
-        node[0],
-        node[1])
-      output = output + "    <name>%d</name>\n" % count
-      output = output + "   </rtept>\n"
-      count = count + 1
-    output = output + " </rte>\n</gpx>\n"
-  
+  for i in route:
+    node = data.rnodes[i]
+    output = output + "%d,%f,%f\n" % ( \
+      i,
+      node[0],
+      node[1])
   return(output)
 
-if __name__ == "__main__":  
-  try:
-    print routeToGpx(
-      float(sys.argv[1]),
-      float(sys.argv[2]),
-      float(sys.argv[3]),
-      float(sys.argv[4]),
-      sys.argv[5],
-      "Route",
-      "track")
-  except IndexError:
-    sys.stderr.write("Usage: routeAsGpx.py lat1 lon1 lat2 lon2 transport\n")
+def routeToCSVFile(lat1,lon1,lat2,lon2, transport, filename):
+  f = open(filename,'w')
+  f.write(routeToCSV(lat1,lon1,lat2,lon2, transport))
+  f.close()
+
+if __name__ == "__main__":
+  print routeToCSV(
+    52.2181,
+    0.1162,
+    52.2184,
+    0.1427,
+    "cycle")
